8828588657e3c2a399d3effd5f0c0f813e91a686
Philedo🦖 <seahsongli@gmail.com>
2025-08-20 23:32:46 +0800

    Update name of chatbot and .gitignore

---
+++ src/main/java/Monty.java
@@ -0,0 +1,15 @@
+public class Monty {
+    public static void main(String[] args) {
+        String logo = " __  __             _          \n"
+             + "|  \\/  | ___  _ __ | |_ _   _  \n"
+             + "| |\\/| |/ _ \\| '_ \\| __| | | | \n"
+             + "| |  | | (_) | | | | |_| |_| | \n"
+             + "|_|  |_|\\___/|_| |_|\\__|\\__,_| \n";
+        System.out.println("Hello from\n" + logo);
+
+        String name = "Monty";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do you for?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
