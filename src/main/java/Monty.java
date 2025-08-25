8e2e2f68a3b58c3d545cdb21fd290319ef7b7262
Philedo🦖 <seahsongli@gmail.com>
2025-08-25 15:52:32 +0800

    Include option to exit chatbot.

--- src/main/java/Monty.java
+++ src/main/java/Monty.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Monty {
     public static void main(String[] args) {
         String logo = " __  __             _          \n"
@@ -9,7 +11,21 @@ public class Monty {
 
         String name = "Monty";
         System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do you for?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+        sc.close();
     }
 }
