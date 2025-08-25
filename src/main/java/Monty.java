4f7e8b524e7892ef54cdb9e12ae1046da75246d0
Philedo🦖 <seahsongli@gmail.com>
2025-08-25 16:14:32 +0800

    Add option to mark and unmark task as done

--- src/main/java/Monty.java
+++ src/main/java/Monty.java
@@ -1,5 +1,32 @@
 import java.util.Scanner;
 
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false; // default not done
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + description;
+    }
+}
+
 public class Monty {
     public static void main(String[] args) {
         String logo = " __  __             _          \n"
@@ -14,7 +41,7 @@ public class Monty {
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         while (true) {
             String input = sc.nextLine();
@@ -23,19 +50,34 @@ public class Monty {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            }
-            else if (input.equals("list")) {
+            } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
-            }
-            else {
-                // add task to array
-                tasks[taskCount] = input;
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println("____________________________________________________________");
+                }
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markNotDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + tasks[index]);
+                    System.out.println("____________________________________________________________");
+                }
+            } else {
+                tasks[taskCount] = new Task(input);
                 taskCount++;
-
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
